5e4b1dde65d10fb034f58063e1ffae9977027b6c
jiangtao <jiangtao@wesai.com>
2018-04-11 19:51:27 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="hello">hello , this is my vue-cli</div>
+</template>
+
+<script>
+    export default {
+        name: "app"
+    }
+</script>
+
+<style scoped>
+    .hello {
+        font-size: 20px;
+    }
+
+</style>
